9ef370c5ad4a284098416da92d297e060bfe16e3
Jackson Yelinek <jackyelinek@gmail.com>
2021-06-27 18:56:15 -0500

    Updated the song select menu so that you can hold down a gamepad button to quickly scroll through the songs, rather than either hitting a button a lot of times, or needing to hit an arrow key.

--- TJAPlayer3/Stages/05.SongSelect/CStage選曲.cs
+++ TJAPlayer3/Stages/05.SongSelect/CStage選曲.cs
@@ -535,20 +535,22 @@ namespace TJAPlayer3
                             }
                             #endregion
                             #region [ Up ]
-                            this.ctキー反復用.Up.tキー反復( TJAPlayer3.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.LeftArrow ), new CCounter.DGキー処理( this.tカーソルを上へ移動する ) );
+                            this.ctキー反復用.Up.tキー反復( TJAPlayer3.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.LeftArrow ) 
+								|| TJAPlayer3.Pad.b押されている(E楽器パート.DRUMS, Eパッド.LBlue), new CCounter.DGキー処理( this.tカーソルを上へ移動する ) );
 							//this.ctキー反復用.Up.tキー反復( CDTXMania.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.UpArrow ) || CDTXMania.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.LeftArrow ), new CCounter.DGキー処理( this.tカーソルを上へ移動する ) );
-							if ( TJAPlayer3.Pad.b押された( E楽器パート.DRUMS, Eパッド.LBlue ) )
-							{
-								this.tカーソルを上へ移動する();
-							}
+							//if ( TJAPlayer3.Pad.b押された( E楽器パート.DRUMS, Eパッド.LBlue))
+							//{
+							//	this.tカーソルを上へ移動する();
+							//}
 							#endregion
 							#region [ Down ]
-							this.ctキー反復用.Down.tキー反復( TJAPlayer3.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.RightArrow ), new CCounter.DGキー処理( this.tカーソルを下へ移動する ) );
+							this.ctキー反復用.Down.tキー反復( TJAPlayer3.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.RightArrow) 
+								|| TJAPlayer3.Pad.b押されている(E楽器パート.DRUMS, Eパッド.RBlue), new CCounter.DGキー処理( this.tカーソルを下へ移動する ) );
 							//this.ctキー反復用.Down.tキー反復( CDTXMania.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.DownArrow ) || CDTXMania.Input管理.Keyboard.bキーが押されている( (int) SlimDX.DirectInput.Key.RightArrow ), new CCounter.DGキー処理( this.tカーソルを下へ移動する ) );
-							if ( TJAPlayer3.Pad.b押された( E楽器パート.DRUMS, Eパッド.RBlue ) )
-							{
-								this.tカーソルを下へ移動する();
-							}
+							//if ( TJAPlayer3.Pad.b押された( E楽器パート.DRUMS, Eパッド.RBlue))
+							//{
+							//	this.tカーソルを下へ移動する();
+							//}
 							#endregion
 							#region [ Upstairs ]
 							if ( ( ( this.act曲リスト.r現在選択中の曲 != null ) && ( this.act曲リスト.r現在選択中の曲.r親ノード != null ) ) && ( TJAPlayer3.Pad.b押された( E楽器パート.DRUMS, Eパッド.FT ) || TJAPlayer3.Pad.b押されたGB( Eパッド.Cancel ) ) )
